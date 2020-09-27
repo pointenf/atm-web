5d3aecfce4e78c0c2f7bbf9fe785ac47eba517df
Siwakorn Anansirikajorn <siwakorn.an@ku.th>
2020-09-27 16:18:07 +0700

    ... your own comment ...

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -8,6 +8,7 @@ import th.ac.ku.atm.model.Customer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class CustomerService {
@@ -25,10 +26,11 @@ public class CustomerService {
 
     public Customer findCustomer(int id) {
         try {
-            return repository.findById(id);
-        } catch (EmptyResultDataAccessException e) {
+            return repository.findById(id).get();
+        } catch (NoSuchElementException e) {
             return null;
         }
+
     }
 
     public List<Customer> getCustomers() {
